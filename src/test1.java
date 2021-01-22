91c7c544871874c0a209e0431ed9768c86a3977a
sbsbsb123 <1605069214@qq.com>
2021-01-22 17:26:25 +0800

    lbs第一次提交

--- src/test1.java
+++ src/test1.java
@@ -2,5 +2,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("11111");
         System.out.println("2222222");
+        System.out.println("999999999999");
+        System.out.println("33333333333");
     }
 }
